fb80d76a41dff1ff2495c73b1fa587312f234b3d
Dima <dimonbacsa@gmail.com>
2025-10-09 16:53:12 +0300

    taskList

--- PROJECTS/01-PROJECT-TaskList/start/taskList/src/App.jsx
+++ PROJECTS/01-PROJECT-TaskList/start/taskList/src/App.jsx
@@ -1,5 +1,117 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [openSection, setOpenSection] = useState({
+    taskList: false,
+    tasks: true,
+    completed: true,
+  });
+
+  function toggleSection(section) {
+    setOpenSection((prev) => ({ ...prev, [section]: !prev[section] }));
+    console.log(openSection);
+  }
+
+  return (
+    <div className="app">
+      <div className="task-container">
+        <h1 className="">Task List with priority</h1>
+        <button
+          className={`close-button ${openSection.taskList ? "open" : ""}`}
+          onClick={() => toggleSection("taskList")}
+        >
+          +
+        </button>
+        {openSection.taskList && <TaskForm />}
+      </div>
+
+      <div className="task-container">
+        <h2>Tasks</h2>
+        <button
+          className={`close-button ${openSection.tasks ? "open" : ""}`}
+          onClick={() => toggleSection("tasks")}
+        >
+          +
+        </button>
+        <div className="sort-controls">
+          <button className="sort-button">By Date</button>
+          <button className="sort-button">By Priority</button>
+        </div>
+        {openSection.tasks && <TaskList />}
+      </div>
+
+      <div className="completed-task-container">
+        <h2>Completed Tasks</h2>
+        <button
+          className={`close-button ${openSection.completed ? "open" : ""}`}
+          onClick={() => toggleSection("completed")}
+        >
+          +
+        </button>
+        {openSection.completed && <CompletedTaskList />}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
+function TaskForm() {
+  return (
+    <form action="" className="task-form">
+      <input type="text" value={""} placeholder="Task title" required />
+      <select value={""}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <input value={""} type="datetime-local" required />
+      <button type="submit">Add task</button>
+    </form>
+  );
+}
+
+function TaskList() {
+  return (
+    <ul className="task-list">
+      <TaskItem />
+    </ul>
+  );
+}
+
+function CompletedTaskList() {
+  return (
+    <ul className="completed-task-list">
+      <TaskItem />
+    </ul>
+  );
+}
+
+function TaskItem() {
+  return (
+    <li className="task-item">
+      <div className="task-info">
+        <div>
+          Title <strong>Medium</strong>
+        </div>
+        <div className="task-deadline">Due: {new Date().toLocaleString()}</div>
+      </div>
+      <div className="task-buttons">
+        <button className="complete-button">Complete</button>
+        <button className="delete-button">Delete</button>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Technologies and React concepts used: React, JSX, props, useState,
+        component composition,conditional rendering, array methods (map,
+        filter), event handling.
+      </p>
+    </footer>
+  );
+}
 export default App;
