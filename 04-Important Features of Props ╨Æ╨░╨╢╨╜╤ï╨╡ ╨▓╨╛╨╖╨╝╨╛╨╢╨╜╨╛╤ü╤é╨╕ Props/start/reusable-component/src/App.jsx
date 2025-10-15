6afcd81d39a9b748f9c7b695acb1faa7250d955b
Dima <dimonbacsa@gmail.com>
2025-10-15 15:36:36 +0300

    reusable-component update(15.10.2025)

--- 04-Important Features of Props ╨Æ╨░╨╢╨╜╤ï╨╡ ╨▓╨╛╨╖╨╝╨╛╨╢╨╜╨╛╤ü╤é╨╕ Props/start/reusable-component/src/App.jsx
+++ 04-Important Features of Props ╨Æ╨░╨╢╨╜╤ï╨╡ ╨▓╨╛╨╖╨╝╨╛╨╢╨╜╨╛╤ü╤é╨╕ Props/start/reusable-component/src/App.jsx
@@ -5,8 +5,8 @@ import Header from "./components/Header";
 import Instruction from "./components/Instruction";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-
-
+import Section from "./components/Section";
+import Button from "./components/Button";
 
 function App() {
   const [showInstructions, setShowInstructions] = useState(false);
@@ -15,19 +15,44 @@ function App() {
     setShowInstructions((prev) => !prev);
   };
 
+  function handleClick() {
+    alert("Click");
+  }
+
   return (
     <div className="app">
-      <Header showInstructions = {showInstructions} toggleInstructions = {toggleInstructions}/>
+      <Header
+        showInstructions={showInstructions}
+        toggleInstructions={toggleInstructions}
+      />
 
-      <Instruction showInstructions = {showInstructions}/>
+      <Instruction showInstructions={showInstructions} />
 
-      <Main/>
+      <Main>
+        <Section title="Variants">
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="danger">Danger</Button>
+          <Button variant="gradient">Gradient</Button>
+          <Button variant="outline">Outline</Button>
+        </Section>
+        <Section title="Sizes">
+          <Button size="large">Large</Button>
+          <Button size="medium">Medium</Button>
+          <Button size="small">Small</Button>
+        </Section>
+        <Section title="Button States">
+          <Button isDisabled>Disabled</Button>
+          <Button>Default</Button>
+        </Section>
+        <Section title="Full width button">
+          <Button fullWidth>Full width</Button>
+        </Section>
+      </Main>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
-
-
 
 export default App;
--- 04-Important Features of Props ╨Æ╨░╨╢╨╜╤ï╨╡ ╨▓╨╛╨╖╨╝╨╛╨╢╨╜╨╛╤ü╤é╨╕ Props/start/reusable-component/src/components/Button.jsx
+++ 04-Important Features of Props ╨Æ╨░╨╢╨╜╤ï╨╡ ╨▓╨╛╨╖╨╝╨╛╨╢╨╜╨╛╤ü╤é╨╕ Props/start/reusable-component/src/components/Button.jsx
@@ -1,17 +1,20 @@
 export default function Button({
-    text = "Some text",
-    icon,
-    size = "medium",
-    variant = "primary", 
-    fullWidth = false,
-    isDisabled = false,
-    onClick
+  text = "Some text",
+  size = "medium",
+  variant = "primary",
+  fullWidth = false,
+  isDisabled = false,
+  onClick,
+  children,
 }) {
-    return (
-        <button className={`button ${variant} ${size} ${isDisabled ? "disabled" : ""} ${fullWidth ? "full-width" : ""}`} onClick = {onClick}>
-            {icon && <span>{icon}</span>}
-            {text}
-        </button>
-    )
+  return (
+    <button
+      className={`button ${variant} ${size} ${isDisabled ? "disabled" : ""} ${
+        fullWidth ? "full-width" : ""
+      }`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
-    
