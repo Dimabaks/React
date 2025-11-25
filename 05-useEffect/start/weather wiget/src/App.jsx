20e58ed588152640230beaa0d5691eb6831802b3
Dmitrii <dimonbacsa@gmail.com>
2025-11-25 18:20:07 +0200

    update 25.11.2025

--- 05-useEffect/start/weather wiget/src/App.jsx
+++ 05-useEffect/start/weather wiget/src/App.jsx
@@ -1,24 +1,51 @@
 import { useState, useEffect } from "react";
 import "./index.css";
 
+const KEY = "6c251eec568a46dea19173345252610";
+
 function App() {
+  const [city, setCity] = useState("Chisinau");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+      const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);
+
+      if (!res.ok) {
+        throw new Error(`Not valid city name ${res.status} ${res.statusText}`)
+      }
+
+      const data = await res.json();
+      console.log(data);
+      setWeatherData(data);
+      } catch(err) {
+        console.log(err)
+        setError(err.message);
+      }
+    }
+    getData();
+  }, [])
+
+
   return (
     <div className="app">
       <div className="widget-container">
         <div className="weather-card-container">
-          <h1 className="app-title">Weather Widget</h1>
+          <h1 className="app-title">{error}Weather Widget</h1>
           <div className="search-container">
             <input type="text" placeholder="Enter city name" className="search-input" />
           </div>
         </div>
         <div className="weather-card">
-          <h2>Moscow, Russia</h2>
-          <img src="" alt="icon" className="weather-icon" />
-          <p className="temperature">11°C</p>
-          <p className="condition">rainy</p>
+          <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+          <img src={weatherData?.current.condition.icon} alt="icon" className="weather-icon" />
+          <p className="temperature">{Math.round(weatherData?.current.temp_c)}C</p>
+          <p className="condition">{weatherData?.current.condition.text}</p>
           <div className="weather-details">
-            <p>Humidity: 20%</p>
-            <p>Wind: 22 km/h</p>
+            <p>Humidity: {weatherData?.current.humidity}%</p>
+            <p>Wind: {weatherData?.current.wind_kph} km/h</p>
           </div>
         </div>
       </div>
